efb07c73715675132035308f658f9f754f2c5d56
Thomas Hertleif <thomas@hertleif.de>
2015-08-29 20:32:15 +0200

    Add a MainController

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,10 +1,30 @@
 package controller;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import view.*;
+
+import view.*;
+
 
 public class MainController {
-
-	public static void main(String[] args) {
-		//TODO: Move main from MainView
-
+	
+	private MainView mainView;
+	
+	public MainController(MainView MainView) {
+		this.mainView = MainView;
+	
+		this.mainView.setNewNoteListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("New Note will be made");
+				
+				mainView.setscrollPaneContent(new view.NewNotePanel());
+				
+			}
+		});
 	}
-
+	
+	
+	
 }
