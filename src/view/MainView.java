f293659a58d8c1cc152e157715bd4f9966f49f5d
Thomas Hertleif <thomas@hertleif.de>
2015-08-29 22:35:21 +0200

    Add Info Panel
    It shows the version and the author

--- src/view/MainView.java
+++ src/view/MainView.java
@@ -8,7 +8,13 @@ import net.miginfocom.swing.MigLayout;
 import javax.swing.JTextField;
 import javax.swing.JScrollPane;
 import javax.swing.JButton;
+
+import java.awt.Component;
 import java.awt.event.ActionListener;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 
 public class MainView {
 
@@ -16,6 +22,9 @@ public class MainView {
 	private JTextField txtSearch;
 	private JButton btnNewNote;
 	private JScrollPane scrollPane = new JScrollPane();
+	private JMenuBar menuBar;
+	private JMenu mnHelp;
+	private JMenuItem mnAbout;
 
 	public MainView() {
 		initialize();
@@ -38,6 +47,21 @@ public class MainView {
 
 		frame.getContentPane().add(scrollPane, "cell 0 1 2 1,grow");
 		scrollPane.setViewportView(new NoteTable());
+		
+		menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+		
+		mnHelp = new JMenu("Hilfe");
+		menuBar.add(mnHelp);
+		
+		mnAbout = new JMenuItem("\u00DCber");
+		mnHelp.add(mnAbout);
+		
+		mnAbout.addActionListener((e) -> {
+			Component infoFrame = null;
+			JOptionPane.showMessageDialog(infoFrame, "Coffee Rabbit Notes v0.0.0 von Thomas Hertleif", "Über",
+				JOptionPane.PLAIN_MESSAGE);
+		});
 		
 		//TODO: Auto update the View!
 		frame.setVisible(true);
