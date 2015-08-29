7dd782d4a9b15809fec8fb4089b92fad6c280ac4
Thomas Hertleif <thomas@hertleif.de>
2015-08-29 20:30:10 +0200

    Update Note Object

--- src/model/note.java
+++ src/model/note.java
@@ -4,19 +4,14 @@ import java.util.Date;
 
 public class note {
 
+	private String title;
 	private Date createDate;
 	private Date changeDate;
 	private byte priority;
 	private String text;
 
-	public note(String note) {
-		this.createDate = new Date();
-		this.changeDate = new Date();
-		this.priority = 1; // 0 = low 1 = normal 2 = high
-		this.text = note;
-	}
-
-	public note(byte prio, String note) {
+	public note(String title, byte prio, String note) {
+		this.title = title;
 		this.createDate = new Date();
 		this.changeDate = new Date();
 		this.priority = prio; // 0 = low 1 = normal 2 = high
