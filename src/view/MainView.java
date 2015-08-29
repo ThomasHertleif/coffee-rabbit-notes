86fb7fcf3ac296edf84e2f65121f31057dc7f6b5
Thomas Hertleif <thomas@hertleif.de>
2015-08-29 20:30:54 +0200

    Update MainView

--- src/view/MainView.java
+++ src/view/MainView.java
@@ -1,45 +1,56 @@
 package view;
 
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JTextField;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class MainView {
 
 	private JFrame frame;
+	private JTextField txtSearch;
+	private JButton btnNewNote;
+	private JScrollPane scrollPane = new JScrollPane();
 
-	/**
-	 * Launch the application.
-	 */
-	
-	//TODO: Move to MainController
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					MainView window = new MainView();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the application.
-	 */
 	public MainView() {
 		initialize();
 	}
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
 	private void initialize() {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new MigLayout("", "[grow][grow]", "[][grow]"));
+		
+		btnNewNote = new JButton("Neue Notiz");
+
+		frame.getContentPane().add(btnNewNote, "cell 0 0");
+		
+		txtSearch = new JTextField();
+		txtSearch.setText("Suche");
+		frame.getContentPane().add(txtSearch, "cell 1 0,growx");
+		txtSearch.setColumns(10);
+
+		frame.getContentPane().add(scrollPane, "cell 0 1 2 1,grow");
+		
+		//TODO: Auto update the View!
+		frame.setVisible(true);
 	}
+	
+	public void setNewNoteListener(ActionListener l) {
+		this.btnNewNote.addActionListener(l);
+	}
+	
+	public void setscrollPaneContent (JPanel newPanel) {
+		scrollPane.setViewportView(newPanel);
+	}
+	
+	
 
 }
