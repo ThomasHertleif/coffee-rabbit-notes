aa24bb8cd8493620f556d62cee8488c0009b28b2
Thomas Hertleif <thomas@hertleif.de>
2015-08-30 20:05:32 +0200

    Litte Update

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
-import view.*;
-
+import model.NoteStore;
 
 public class Main {
 
 	static view.MainView mainView;
 	static controller.MainController mainController;
-	
+	static model.NoteStore noteStore;
+
 	public static void main(String[] args) {
 		
+		noteStore = new NoteStore();
 		mainView = new view.MainView();
-		mainController = new controller.MainController(mainView);
+		mainController = new controller.MainController(mainView, noteStore);
 
 	}
-
 }
