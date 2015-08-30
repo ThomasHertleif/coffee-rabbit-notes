e402bcf78e1e36ed88fe6ce9f86823d7d51e7eb2
Thomas Hertleif <thomas@hertleif.de>
2015-08-30 12:47:12 +0200

    Add FileWriter Class

---
+++ src/model/FileWriter.java
@@ -0,0 +1,15 @@
+package model;
+
+public class FileWriter {
+
+	public FileWriter() {
+		// TODO: Remove debug info
+		System.out.println("New FileWriter");
+	}
+	
+	public void writeToDisk(Note note) {
+		// TODO: Remove debug info
+		System.out.println("Writing note to Disk");
+	}
+	
+}
