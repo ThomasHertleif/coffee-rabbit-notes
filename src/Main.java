02cfbcc27f761fc00c8a5f89c449741a91a26400
Thomas Hertleif <thomas@hertleif.de>
2015-09-05 14:47:31 +0200

    Update Main Class

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
-import model.NoteStore;
+import model.*;
 
 public class Main {
 
 	static view.MainView mainView;
 	static controller.MainController mainController;
-	static model.NoteStore noteStore;
+	static model.Store<Note> noteStore;
 
 	public static void main(String[] args) {
 		
-		noteStore = new NoteStore();
+		noteStore = new Store<Note>();
 		mainView = new view.MainView();
 		mainController = new controller.MainController(mainView, noteStore);
 
