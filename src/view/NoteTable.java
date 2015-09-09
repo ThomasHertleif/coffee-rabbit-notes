ed615b8516ebfbbb1c079a719adc636ad810df28
Thomas Hertleif <thomas@hertleif.de>
2015-09-09 19:20:23 +0200

    Finally a (nearly) working JTable

--- src/view/NoteTable.java
+++ src/view/NoteTable.java
@@ -1,28 +1,53 @@
 package view;
 
+import java.io.File;
+
 import javax.swing.JPanel;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+
 import model.*;
 
 public class NoteTable extends JPanel {
-	
-	private NoteTableModel tblModel;
-	
+
 	private JTable tbl_noteList;
 	private Store<Note> noteStore;
-	
+	private Note[] nA;
+
+	private DefaultTableModel dtm;
+
 	public NoteTable(Store<Note> noteStore) {
+
+		this.noteStore = noteStore;
+		dtm = new DefaultTableModel(0, 0);
+
 		tbl_noteList = new JTable();
 		add(tbl_noteList);
-		 
-		
-		this.noteStore = noteStore;
-		//this.tbl_noteList.setTableHeader(tblModel());
-		
-		
+
+		dtm.setColumnIdentifiers(new String[] { "Name", "Erstellt", "Geändert", "Priorität" });
+
+		tbl_noteList.setModel(dtm);
+
 	}
-	
-	public void updateTable() {
+
+	public void updateTable(Store<Note> nStore) {		
+		dtm.addRow(nStore.getAll().toArray());
 		
+		// remove all them rows
+		if (dtm.getRowCount() > 0) {
+			for (int i = dtm.getRowCount() - 1; i > -1; i--) {
+				dtm.removeRow(i);
+			}
+		}
+
+		// this.updateState();
+
+		for (Note note : nStore.getAll()) {
+			dtm.addRow(new Object[] { note.getTitle(), note.getCreateDate(), note.getChangeDate(), note.getPriority() });
+		}
+
+		tbl_noteList.updateUI();
 	}
+
 }
