22ffd1a185a6033cf76471c5ec178562299f9408
Thomas Hertleif <thomas@hertleif.de>
2015-09-09 19:22:18 +0200

    Remove some Zip Imports

--- src/model/NoteWriter.java
+++ src/model/NoteWriter.java
@@ -1,15 +1,58 @@
 package model;
 
 import java.io.*;
+import java.util.stream.Stream;
+import java.util.zip.*;
 
 public class NoteWriter {
 
 	private FileWriter writer;
 	private Note note;
+	private Store<Note> noteStore;
 
-	public NoteWriter() {
+	// TODO: Path Fix
+	public NoteWriter(Store<Note> noteStore) {
+
+		this.noteStore = noteStore;
 
 	}
+
+	public Stream<String[]> serializeNotes(Store<Note> notes) {
+		return notes.getAll().stream().map((note) -> {
+			// FIXME: add uuid field to note and use it instead of title here.
+			String[] nameAndContent = { note.getTitle(), note.cearialize() };
+			return nameAndContent;
+		});
+	}
+
+	public void newNotebook(String name) {
+
+		try {
+			FileOutputStream dest = new FileOutputStream("C:/Users/Thomas/Documents/Notes/" + name + ".crnz");
+			ZipOutputStream zipOut = new ZipOutputStream(new BufferedOutputStream(dest));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	// public void writeToDisk(Stream<String[]> textStream) {
+	// textStream.forEach((nameAndContent) -> {
+	// try {
+	//
+	// FileOutputStream dest = new
+	// FileOutputStream("C:/Users/Thomas/Documents/Notes/" + "jim" + ".crnz");
+	// ZipOutputStream zipOut = new ZipOutputStream(new
+	// BufferedOutputStream(dest));
+	//
+	// ZipEntry e = new ZipEntry(nameAndContent[0]);
+	//
+	// zipOut.putNextEntry(e);
+	//
+	// } catch (Exception e) {
+	// e.printStackTrace();
+	// }
+	// });
+	// }
 
 	public void writeToDisk(Note note) {
 		// TODO: Remove debug info
@@ -18,14 +61,15 @@ public class NoteWriter {
 		this.note = note;
 
 		try {
-			writer = new FileWriter("C:/Users/Thomas/Documents/Notes/" + note.getTitle() + ".txt");
-			writer.write(note.getPriority() + "§" + note.getCreateDate() + "§" + note.getChangeDate() + "§");
-			writer.write(note.getText());
+			writer = new FileWriter("C:/Users/" + System.getProperty("user.name").toString() + "/Documents/Notes/"
+					+ note.getTitle() + ".crnz");
+			writer.write(note.cearialize());
 			writer.flush();
 			writer.close();
 			System.out.println("File writen to the disk");
 		} catch (IOException e) {
 			e.printStackTrace();
+			System.out.println("Writer error");
 		}
 	}
 }
