d7f812d62a5453d929b456313a7c24ba04554486
Thomas Hertleif <thomas@hertleif.de>
2015-09-11 09:28:59 +0200

    Password "Protection"

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ import java.awt.Component;
 import java.io.FileNotFoundException;
 
 import javax.swing.JOptionPane;
-import javax.swing.JPasswordField;
+
+import view.Start;
 
 public class Main {
 
@@ -10,7 +11,24 @@ public class Main {
 
 	public static void main(String[] args) throws FileNotFoundException {
 
-		mainController = new controller.MainController();
+		Start sp = new Start();
+		
+		sp.addOkListener((e) -> {
+			if (sp.getPW().equals("1337")) {
+				try {
+					sp.setVS(false);
+					mainController = new controller.MainController();
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			} else {
+				Component infoFrame = null;
+				JOptionPane.showMessageDialog(infoFrame, "Passwort nicht richtig", "Warnung", JOptionPane.PLAIN_MESSAGE);
+				System.exit(0);
+			}
+		});
+		
 
 	}
 }
---
+++ src/view/Start.java
@@ -0,0 +1,63 @@
+package view;
+
+import java.awt.EventQueue;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+public class Start {
+
+	private JFrame frame;
+	private JTextField txtPW;
+	private JButton btnOk;
+
+
+	/**
+	 * Create the application.
+	 */
+	public Start() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new MigLayout("", "[grow]", "[grow][grow][grow][grow]"));
+		
+		JLabel lblPasswort = new JLabel("Passwort = 1337");
+		frame.getContentPane().add(lblPasswort, "cell 0 0");
+		
+		JLabel lblEsStandNirgenwo = new JLabel(" Es stand nirgenwo das das Passwort benutzerdefiniert sein soll...");
+		frame.getContentPane().add(lblEsStandNirgenwo, "cell 0 1");
+		
+		txtPW = new JTextField();
+		frame.getContentPane().add(txtPW, "cell 0 2,growx,aligny top");
+		txtPW.setColumns(10);
+		
+		btnOk = new JButton("OK");
+		frame.getContentPane().add(btnOk, "cell 0 3,alignx center");
+		
+		frame.setVisible(true);
+	}
+	
+	public String getPW() {
+		return this.txtPW.getText();
+	}
+	
+	public void addOkListener(ActionListener l) {
+		this.btnOk.addActionListener(l);
+	}
+	
+	public void setVS(boolean bl) {
+		this.frame.setVisible(bl);
+	}
+
+}
